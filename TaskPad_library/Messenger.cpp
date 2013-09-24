4010d5711bf9fcfd8429988d78001c55473b9bba
Thyagesh M <thyageshm@gmail.com>
2013-09-24 14:48:50 +0800

    Implemented Messenger class
    files added:
            - Messenger.h
            - Messenger.cpp

---
+++ TaskPad_library/Messenger.cpp
@@ -0,0 +1,35 @@
+#include "Messenger.h"
+#include "Task.h"
+
+using namespace std;
+
+// constructors
+Messenger::Messenger(string errorMsg, STATUS status, list<Task> result)
+{
+	this->_errorMsg = errorMsg;
+	this->_status = status;
+	this->_resultList = result;
+}
+
+//getter methods
+		
+string Messenger::getErrorMsg()
+{
+	return this->_errorMsg;
+}
+STATUS Messenger::getStatus()
+{
+	return this->_status;
+}
+list<Task> Messenger::getIntermediateList()
+{
+	return this->_resultList;
+}
+list<Task> Messenger::getResultList()
+{
+	return this->getIntermediateList();
+}
+list<Task> Messenger::getList()
+{
+	return this->getIntermediateList();
+}
