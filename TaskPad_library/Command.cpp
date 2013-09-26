3fb322a9412e34132b147e1a1574652147f2dbbb
Ashray Jain <ashrayj11@gmail.com>
2013-09-26 21:58:01 +0800

    Merge

--- TaskPad_library/Command.cpp
+++ TaskPad_library/Command.cpp
@@ -14,41 +14,52 @@
 
 #include "Command.h"
 
-std::string Command::defaultSyncProvider = "GCal";
+const bool					Command::DEFAULT_FLAG			= Command::UNCHANGED;
+const Command::COMMAND_TYPE Command::DEFAULT_COMMAND_TYPE	= Command::UNDEFINED;
+const Command::PRIORITY		Command::DEFAULT_PRIORITY		= Command::MEDIUM;
+const bool					Command::DEFAULT_TASK_STATE		= Command::UNDONE;//undone
+const Command::TASK_TYPE	Command::DEFAULT_TASK_TYPE		= Command::FLOATING;
+const std::string			Command::DEFAULT_STRING			= "";
+const std::time_t			Command::DEFAULT_TIME			= -1;
+const int					Command::DEFAULT_INDEX			= -1;
 
 void Command::init()
 {
-	std::time_t currentTime = std::time(NULL);
-
 	//flags init
-	FLAG_index = false;
-	FLAG_exact = false;
-	FLAG_name = false;
-	FLAG_due = false;
-	FLAG_from = false;
-	FLAG_to = false;
-	FLAG_location = false;
-	FLAG_participants = false;
-	FLAG_note = false;
-	FLAG_priority = false;
-	FLAG_tags = false;
-	FLAG_remindTime = false;
-	FLAG_taskState = false;
+	FLAG_index			= DEFAULT_FLAG;
+	FLAG_exact			= DEFAULT_FLAG;
+	FLAG_name			= DEFAULT_FLAG;
+	FLAG_optName		= DEFAULT_FLAG;
+	FLAG_due			= DEFAULT_FLAG;
+	FLAG_from			= DEFAULT_FLAG;
+	FLAG_to				= DEFAULT_FLAG;
+	FLAG_location		= DEFAULT_FLAG;
+	FLAG_participants	= DEFAULT_FLAG;
+	FLAG_note			= DEFAULT_FLAG;
+	FLAG_priority		= DEFAULT_FLAG;
+	FLAG_tags			= DEFAULT_FLAG;
+	FLAG_remindTime		= DEFAULT_FLAG;
+	FLAG_taskState		= DEFAULT_FLAG;
+	FLAG_createdTime	= DEFAULT_FLAG;
+	FLAG_taskType		= DEFAULT_FLAG;
+	FLAG_syncProviderName = DEFAULT_FLAG;
 
 	//fields init
-	_type = UNDEFINED;
-	_index = -1;
-	_name = "";
-	_optName = "";
-	_dueDate = currentTime;
-	_fromDate = currentTime;
-	_toDate = currentTime;
-	_location = "";
-	_participants = "";
-	_note = "";
-	_priority = MEDIUM;
-	_tags = "";
-	_remindTime = currentTime;
-	_syncProviderName = defaultSyncProvider;
-	_taskState = false;//undone
+	_type				= DEFAULT_COMMAND_TYPE;
+	_index				= DEFAULT_INDEX;
+	_name				= DEFAULT_STRING;
+	_optName			= DEFAULT_STRING;
+	_dueDate			= DEFAULT_TIME;
+	_fromDate			= DEFAULT_TIME;
+	_toDate				= DEFAULT_TIME;
+	_location			= DEFAULT_STRING;
+	_participants		= DEFAULT_STRING;
+	_note				= DEFAULT_STRING;
+	_priority			= DEFAULT_PRIORITY;
+	_tags				= DEFAULT_STRING;
+	_remindTime			= DEFAULT_TIME;
+	_syncProviderName	= DEFAULT_STRING;
+	_taskState			= DEFAULT_TASK_STATE;
+	_createdTime		= DEFAULT_TIME;
+	_taskType			= DEFAULT_TASK_TYPE;
 }
