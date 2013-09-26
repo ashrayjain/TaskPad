b7b589590b74782242e681dc443e373be192f0e8
Thyagesh <Thyagesh@localhost>
2013-09-26 17:33:52 +0800

    Manager Class
    files edited:
            - Manager.cpp
    
    Continued implementation

--- TaskPad_library/Manager.cpp
+++ TaskPad_library/Manager.cpp
@@ -1,10 +1,19 @@
 #include "Manager.h"
-#include <string>
- using namespace std;
+#include "Storage.h"
 
- Manager::Manager()
- {
-	 this->_executor = new Executor;
-	 this->_interpreter = new Interpreter;
-	 this->_storage = new Storage;
- }
+
+Manager::Manager()
+{
+	this->_executor = new Executor;
+	this->_interpreter = new Interpreter;
+	this->_storage = new Storage;
+}
+
+Messenger Manager::processCommand(string newCommand)
+{
+	
+}
+
+void Manager::resetStatus()
+{
+}
