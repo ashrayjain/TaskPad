c68573a439eb037a0f2c3cd871afbbad6b2a6df9
Ashray Jain <ashrayj11@gmail.com>
2013-09-26 21:48:54 +0800

    Added Executor class header file
    Files Added:
            - Executor.h
            - Executor.cpp

---
+++ TaskPad_library/mainwindow.cpp
@@ -0,0 +1,42 @@
+#include <QMouseEvent>
+#include <qlist.h>
+#include "mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+	: QMainWindow(parent)
+{
+	ui.setupUi(this);
+	customisedUi();
+	QObject::connect(ui.CloseButton, SIGNAL(clicked()), this, SLOT(close()));
+}
+
+MainWindow::~MainWindow()
+{
+
+}
+
+void MainWindow::customisedUi(){
+	this->setWindowFlags(Qt::FramelessWindowHint);
+	this->setAttribute(Qt::WA_TranslucentBackground, true);
+	//magic number
+	ui.TaskList->setGeometry(9, 135, 782, ui.TaskList->topLevelItemCount() * 48 + 10);
+	ui.TaskList->setRootIsDecorated(false);
+	ui.TaskList->setFrameStyle(QFrame::NoFrame);
+	ui.TaskList->header()->setStretchLastSection(false);
+	ui.TaskList->header()->setSectionResizeMode(1, QHeaderView::Stretch);
+	ui.TaskList->header()->resizeSection(0, 80);
+	ui.TaskList->header()->resizeSection(2, 150);
+	ui.TaskList->header()->resizeSection(3, 150);
+}
+
+void MainWindow::mousePressEvent(QMouseEvent *event){
+	this->windowPosition = this->pos();
+	this->mousePressPosition = event->globalPos();
+}
+
+void MainWindow::mouseMoveEvent(QMouseEvent *event){
+	this->mouseMovePosition = event->globalPos();
+	QPoint distanceToMove = this->mouseMovePosition - this->mousePressPosition + this->windowPosition;
+
+	this->move(distanceToMove);   
+}
