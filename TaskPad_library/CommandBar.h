0599cec549e90d4c4c3371994a487b7e17fa461d
KAI XIE <kai33@users.noreply.github.com>
2013-09-28 18:17:34 +0800

    raw Auto Complete(ac) 1. support one command ac 2. support keywords ac 3. support single quotation ac
    but ugly coding

--- TaskPad_library/CommandBar.h
+++ TaskPad_library/CommandBar.h
@@ -6,6 +6,7 @@
 #include <QShortcut>
 #include <QtAlgorithms>
 #include <QKeyEvent>
+#include <QTextBlock>
 
 class CommandBar: public QTextEdit
 {
@@ -54,7 +55,7 @@ public:
 		completer->setCaseSensitivity(Qt::CaseInsensitive);
 		completer->setWrapAround(true);
 		////////////////////
-		populateModel();
+		produceCommandModel();
 		//connect(completer, SIGNAL(activated(const QString&)), this, SLOT(insertCompletion(const QString&)));
 		connect(this, SIGNAL(textChanged()), this, SLOT(performCompletion()));
 		//(void) new QShortcut(QKeySequence(tr("Ctrl+M", "Complete")), this, SLOT(performCompletion()));
@@ -63,6 +64,10 @@ public:
 	private slots:
 		void performCompletion()
 		{
+			if(containsCommand())
+				produceKeywordModel();
+			else// should only once...
+				produceCommandModel();
 			if(completedAndSelected == false && causedByBackspace == false)
 			{
 				QTextCursor cursor = textCursor();
@@ -71,37 +76,91 @@ public:
 				if (!completionPrefix.isEmpty() &&
 					completionPrefix.at(completionPrefix.length() - 1)
 					.isLetter())
+				{
 					performCompletion(completionPrefix);
+					return;
+				}
+				cursor = textCursor();
+				cursor.movePosition(QTextCursor::Left, QTextCursor::KeepAnchor);
+				const QString newCompletionPrefix = cursor.selectedText();
+				if(newCompletionPrefix== "'")
+				{
+					cursor = textCursor();
+					cursor.movePosition(QTextCursor::Right, QTextCursor::KeepAnchor);
+					if(cursor.selectedText() == "'")
+					{
+						causedByBackspace = true;
+						cursor.removeSelectedText();
+						cursor.insertText(" ");
+						setTextCursor(cursor);
+						return;
+					}
+					insertCompletion("''");
+					causedByBackspace = false;
+					return;
+				}
 			}
 		}
-		private slots:
-			void insertCompletion(const QString &completion, bool singleWord)
-			{
-				QTextCursor cursor = textCursor();
-				int numberOfCharsToComplete = completion.length() -completer->completionPrefix().length();
-				if(numberOfCharsToComplete == 0)
-					return;
-				int insertionPosition = cursor.position();
-				completedAndSelected = true;
-				cursor.insertText(completion.right(numberOfCharsToComplete));
-				cursor.setPosition(insertionPosition);
-				cursor.movePosition(QTextCursor::EndOfWord,
-					QTextCursor::KeepAnchor);
-
-				setTextCursor(cursor);
-			}
 
 private:
+	void insertCompletion(const QString &completion)
+		{
+			QTextCursor cursor = textCursor();
+			int numberOfCharsToComplete = completion.length() - 1;
+			if(numberOfCharsToComplete == 0)
+				return;
+			int insertionPosition = cursor.position();
+			completedAndSelected = true;
+			cursor.insertText(completion.right(numberOfCharsToComplete));
+			cursor.setPosition(insertionPosition);
+			setTextCursor(cursor);
+		}
+
+		void insertCompletion(const QString &completion, bool singleWord)
+		{
+			QTextCursor cursor = textCursor();
+			int numberOfCharsToComplete = completion.length() -completer->completionPrefix().length();
+			if(numberOfCharsToComplete == 0)
+				return;
+			int insertionPosition = cursor.position();
+			completedAndSelected = true;
+			cursor.insertText(completion.right(numberOfCharsToComplete));
+			cursor.setPosition(insertionPosition);
+			cursor.movePosition(QTextCursor::EndOfWord,
+				QTextCursor::KeepAnchor);
+
+			setTextCursor(cursor);
+		}
+	bool containsCommand()
+	{
+		bool result = false;
+		QString currentLine = textCursor().block().text();
+		if(currentLine.startsWith("add", Qt::CaseInsensitive))
+			result = true; 
+		if(currentLine.startsWith("mod", Qt::CaseInsensitive))
+			result = true; 
+		if(currentLine.startsWith("del", Qt::CaseInsensitive))
+			result = true; 
+		if(currentLine.startsWith("find", Qt::CaseInsensitive))
+			result = true; 
+		if(currentLine.startsWith("undo", Qt::CaseInsensitive))
+			result = true; 
+		if(currentLine.startsWith("redo", Qt::CaseInsensitive))
+			result = true; 
+		if(currentLine.startsWith("sync", Qt::CaseInsensitive))
+			result = true; 
+		return result;
+	}
 	void performCompletion(const QString &completionPrefix)
 	{
 		if (completionPrefix != completer->completionPrefix()) {
 			completer->setCompletionPrefix(completionPrefix);
 		}
-		if (completer->completionCount() == 1)
+		if (completer->completionCount() != 0)
 			insertCompletion(completer->currentCompletion(), true);
 	}
 
-	void populateModel()
+	void produceCommandModel()
 	{
 		QStringList strings;
 		strings.push_back("add");
@@ -113,6 +172,29 @@ private:
 		strings.push_back("sync");
 		model->setStringList(strings);
 	}
+
+	void produceKeywordModel()
+	{
+		QStringList strings;
+		strings.push_back("exact");
+		strings.push_back("name");
+		strings.push_back("due");
+		strings.push_back("from");
+		strings.push_back("to");
+		strings.push_back("at");
+		strings.push_back("ppl");
+		strings.push_back("note");
+		strings.push_back("impt");
+		strings.push_back("rt");
+		strings.push_back("done");
+		strings.push_back("undone");
+		strings.push_back("deadline");
+		strings.push_back("timed");
+		strings.push_back("floating");
+		strings.push_back("''");
+		model->setStringList(strings);
+	}
+
 	bool caseInsensitiveLessThan(const QString &a, const QString &b)
 	{
 		return a.compare(b, Qt::CaseInsensitive) < 0;
@@ -120,7 +202,8 @@ private:
 	void keyPressEvent(QKeyEvent*event)
 	{
 		if (event->key() == Qt::Key_Backspace ||
-			event->key() == Qt::Key_Delete)
+			event->key() == Qt::Key_Delete ||
+			event->key() == Qt::Key_Escape)
 			causedByBackspace = true;
 		else
 			causedByBackspace = false;
@@ -162,6 +245,7 @@ private:
 		if (completedAndSelected) {
 			completedAndSelected = false;
 			QTextCursor cursor = textCursor();
+			causedByBackspace = true;
 			cursor.removeSelectedText();
 			setTextCursor(cursor);
 		}
