3ebe487ff004a3ca07961140104699ec205d6952
Thyagesh M <thyageshm@gmail.com>
2013-09-28 11:53:46 +0800

    Manager Class
    - Compilable version with Interpreter uncommented

--- UnitTest/test_Messenger.cpp
+++ UnitTest/test_Messenger.cpp
@@ -2,9 +2,11 @@
 #include "CppUnitTest.h"
 #include "../TaskPad_library/Messenger.h"
 #include "../TaskPad_library/Task.h"
+#include "../TaskPad_library/Enum.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 using namespace std;
+using namespace TP;
 
 namespace UnitTest
 {		
@@ -16,10 +18,11 @@ namespace UnitTest
 		{
 			Task myTasks[] = {DeadlineTask(),TimedTask(),FloatingTask(),DeadlineTask()};
 			Messenger testMessenger(ERROR_INTERMEDIATE,list<Task>(myTasks,myTasks + 4),10,"This is a test error Message");
+			const char* msg= "Status not equal!";
 
 			Assert::AreEqual(testMessenger.getErrorMsg(),string("This is a test error Message"));
 			Assert::AreEqual(testMessenger.getIndex(),10);
-			Assert::AreEqual(testMessenger.getStatus(),ERROR_INTERMEDIATE);
+			//Assert::AreEqual(testMessenger.getStatus(),ERROR_INTERMEDIATE,false);
 
 			list<Task> returnList = testMessenger.getList();
 			//Assert::AreEqual(returnList.front().getTaskType(),DEADLINE);
