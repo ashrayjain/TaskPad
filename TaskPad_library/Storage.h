e8f16d86e06b970cfd6491ff3256348e7b232b19
Thyagesh M <thyageshm@gmail.com>
2013-09-30 20:02:33 +0800

    Enum.h         - made the string arrays static to avoid redefinition ------------------------------------------------- Storage class
    functions defined:
            - convertToString() functions
            - template functions to save each attribute of a task (takes in a template pointer)

--- TaskPad_library/Storage.h
+++ TaskPad_library/Storage.h
@@ -3,6 +3,7 @@
 
 #include<fstream>
 #include<list>
+#include "Enum.h"
 
 //forward definitions
 class Task;
@@ -12,6 +13,7 @@ class TimedTask;
 class FloatingTask;
 
 using namespace std;
+using namespace TP;
 
 class Storage
 {
@@ -25,8 +27,33 @@ class Storage
 		void saveTimedTask(Task* tempTask);
 		void saveFloatingTask(Task* tempTask);
 		void saveString(string line);
+		template <class taskTypePointer> void saveTaskType(taskTypePointer T);
+		template <class taskTypePointer> void saveIndex(taskTypePointer T);
+		template <class taskTypePointer> void saveName(taskTypePointer T);
+		template <class taskTypePointer> void saveDueDate(taskTypePointer T);
+		template <class taskTypePointer> void saveFromDate(taskTypePointer T);
+		template <class taskTypePointer> void saveToDate(taskTypePointer T);
+		template <class taskTypePointer> void saveLocation(taskTypePointer T);
+		template <class taskTypePointer> void saveParticipants(taskTypePointer T);
+		template <class taskTypePointer> void saveNote(taskTypePointer T);
+		template <class taskTypePointer> void savePriority(taskTypePointer T);
+		template <class taskTypePointer> void saveTags(taskTypePointer T);
+		template <class taskTypePointer> void saveReminderTimes(taskTypePointer T);
+		template <class taskTypePointer> void saveState(taskTypePointer T);
 
 		static string convertTimeToString(time_t time);
+
+		// stringconverters
+		string convertToString(unsigned index);
+		string convertToString(TASK_TYPE type);
+		string convertToString(time_t time);
+		string convertToString(list<time_t> timeList);
+		string convertToString(list<string> strList);
+		string convertToString(PRIORITY priority);
+		string convertToString(TASK_STATE state);
+
+		//writers
+		bool writeLineToFile(string line);
 	public:
 		Storage(list<Task>&);
 
