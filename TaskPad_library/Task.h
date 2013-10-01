c9b83f5b00343158f7c3d11c6427960bd9697334
Ashray Jain <ashrayj11@gmail.com>
2013-10-01 19:42:07 +0800

    Added virtual function to Task Class Files Changed:         - Task.h

---
+++ TaskPad_library/Task.h
@@ -0,0 +1,196 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Task.h
+ *
+ *        Version:  1.0
+ *        Created:  09/23/13 17:32:00
+ *
+ *         Author:  ASHRAY JAIN (A0105199B), ashrayj11@gmail.com
+ *   Organization:  NUS, SoC
+ *
+ * =====================================================================================
+ */
+
+
+/*
+ *		How to use these classes
+ *
+ *
+ */
+
+#include <string>
+#include <ctime>
+#include <set>
+#include "Enum.h"
+
+#ifndef TASKPAD_TASK_H
+#define TASKPAD_TASK_H
+
+const std::string INDEX_INVALID_ERROR = "is not a valid index!";
+
+/*
+ *=======================
+ 
+	Task Base class
+ 
+ *=======================
+ */
+
+class Task
+{
+public:
+	Task()						{ defaultTaskInit(); }
+	Task(unsigned indexToPut)	{ defaultTaskInit(indexToPut); }
+	virtual ~Task() {}
+	TP::TASK_TYPE getTaskType() { return _taskType; }
+
+	// Getter Functions common to all Tasks
+	std::string		getName()			{ return _taskName;			}		
+	std::string		getLocation()		{ return _taskLocation;		}
+	std::string		getParticipants()	{ return _taskParticipants;	}
+	std::string		getNote()			{ return _taskNote;			}
+	TP::PRIORITY	getPriority()		{ return _taskPriority;		}
+	std::string		getTags()			{ return _taskTags;			}
+	std::time_t		getRemindTime()		{ return _taskRemindTime;	}	
+	TP::TASK_STATE	getState()			{ return _taskState;		}
+	unsigned		getIndex()			{ return _taskIndex;		}
+
+	// Setter Functions common to all Tasks
+	void setName(std::string newName)					{ _taskName			= newName;			flagName = true;			}	
+	void setLocation(std::string newLocation)			{ _taskLocation		= newLocation;		flagLocation = true;		}
+	void setParticipants(std::string newParticipants)	{ _taskParticipants = newParticipants;	flagParticipant = true;		}
+	void setNote(std::string newNote)					{ _taskNote			= newNote;			flagNote = true;			}
+	void setPriority(TP::PRIORITY newPriority)			{ _taskPriority		= newPriority;		flagPriority = true;		}
+	void setTags(std::string newTags)					{ _taskTags			= newTags;			flagTags = true;			}
+	void setRemindTime(std::time_t newRemindTime)		{ _taskRemindTime	= newRemindTime;	flagRemindTime = true;		}
+	void setState(TP::TASK_STATE newState)				{ _taskState		= newState;			flagState = true;			}
+
+	// Flag Getter Functions common to all Tasks
+	bool getFlagName()			{ return flagName; }
+	bool getFlagLocation()		{ return flagLocation; }
+	bool getFlagParticipant()	{ return flagParticipant; }
+	bool getFlagNote()			{ return flagNote; }
+	bool getFlagPriority()		{ return flagPriority; }
+	bool getFlagTags()			{ return flagTags; }
+	bool getFlagRemindTime()	{ return flagRemindTime; }
+	bool getFlagState()			{ return flagState; }
+
+	
+protected:
+	// Set of All currently used indices
+	static std::set<unsigned> listOfAllIndices;
+
+	// Default Task Initialization
+	void defaultTaskInit();
+	void defaultTaskInit(unsigned indexToPut);
+	void initFlags();
+	void initTaskAttributes();
+	bool isIndexInList(unsigned index) 
+	{ 
+		return listOfAllIndices.find(index) == listOfAllIndices.end();
+	}
+	void setTaskType(TP::TASK_TYPE type) { _taskType = type; }	
+
+	// Task Attributes
+	TP::TASK_TYPE	_taskType;
+	std::string		_taskName;
+	std::time_t		_taskDueDate;
+	std::time_t		_taskFromDate;
+	std::time_t		_taskToDate;
+	std::string		_taskLocation;
+	std::string		_taskParticipants;
+	std::string		_taskNote;
+	TP::PRIORITY	_taskPriority;
+	std::string		_taskTags;
+	std::time_t		_taskRemindTime;
+	TP::TASK_STATE	_taskState;
+	unsigned		_taskIndex;
+
+	// Task Attribute Flags
+	bool flagName;
+	bool flagDueDate;
+	bool flagFromDate;
+	bool flagToDate;
+	bool flagLocation;
+	bool flagParticipant;
+	bool flagNote;
+	bool flagPriority;
+	bool flagTags;
+	bool flagRemindTime;
+	bool flagState;
+};
+
+/*
+ *=======================
+ 
+ Timed Task
+ 
+ *=======================
+ */
+class TimedTask: public Task
+{
+public:
+	TimedTask()
+	{
+		defaultTaskInit();
+		setTaskType(TP::TIMED);
+	}
+
+	// Getter Functions specific to Timed Tasks
+	std::time_t		getFromDate()				{ return _taskFromDate;		}
+	std::time_t		getToDate()					{ return _taskToDate;		}
+
+	// Setter Functions	specific to Timed Tasks
+	void setFromDate(std::time_t newFromDate)	{ _taskFromDate	= newFromDate;	flagFromDate = true; }
+	void setToDate(std::time_t newToDate)		{ _taskToDate	= newToDate;	flagToDate = true; }
+	
+	// Flag Getter Functions specific to Timed Tasks
+	bool getFlagFromDate()						{ return flagFromDate; }
+	bool getFlagToDate()						{ return flagToDate; }
+};
+
+/*
+ *=======================
+ 
+ Floating Task
+ 
+ *=======================
+ */
+class FloatingTask: public Task
+{
+public:
+	FloatingTask()
+	{
+		defaultTaskInit();
+		setTaskType(TP::FLOATING);
+	}
+};
+
+/*
+ *=======================
+ 
+ Deadline Task
+ 
+ *=======================
+ */
+class DeadlineTask: public Task
+{
+public:
+	DeadlineTask()
+	{
+		defaultTaskInit();
+		setTaskType(TP::DEADLINE);
+	}
+
+	// Getter Functions specific to Deadline Tasks
+	std::time_t		getDueDate()			{ return _taskDueDate; }
+	
+	// Setter Functions	specific to Deadline Tasks
+	void setDueDate(std::time_t newDueDate)	{ _taskDueDate		= newDueDate; flagDueDate = true; }
+	
+	// Flag Getter Functions specific to Deadline Tasks
+	bool getFlagDueDate()					{ return flagDueDate; }
+};
+
+#endif
---
+++ TaskPad_library/mainwindow.cpp
@@ -0,0 +1,92 @@
+#include <QMouseEvent>
+#include <QMessageBox>
+#include <QTextBlock>
+#include <QShortcut>
+#include "mainwindow.h"
+#include "lastColumnDelegate.h"
+#include "CommandBar.h"
+
+MainWindow::MainWindow(QWidget *parent)
+	: QMainWindow(parent) 
+{
+	ui.setupUi(this);
+	customisedUi();
+	QObject::connect(ui.CloseButton, SIGNAL(clicked()), this, SLOT(close()));
+	QObject::connect(ui.MinimizeButton, SIGNAL(clicked()), this, SLOT(showMinimized()));
+	QObject::connect(ui.AboutButton, SIGNAL(clicked()), this, SLOT(about()));
+	(void) new QShortcut(QKeySequence(tr("Ctrl+N", "New Task")), this, SLOT(createNewTaskTemplate()));
+	//ui.CommandBar->installEventFilter(this);//filter RETURN
+	ui.CloseButton->installEventFilter(this);//filter MOUSE MOVE
+	ui.MinimizeButton->installEventFilter(this);//filter MOUSE MOVE
+	ui.HelpButton->installEventFilter(this);//filter MOUSE MOVE
+	ui.AboutButton->installEventFilter(this);//filter MOUSE MOVE
+	ui.cmdBar->installEventFilter(this);
+}
+
+MainWindow::~MainWindow()
+{
+
+}
+
+bool MainWindow::eventFilter(QObject* watched, QEvent* event)
+{
+	if(watched == ui.CloseButton || watched == ui.MinimizeButton ||
+		watched == ui.HelpButton || watched == ui.AboutButton)
+	{
+		if(event->type() == QEvent::MouseMove)
+		{
+			return true;
+		}
+	}
+	else if(watched == ui.cmdBar)
+	{
+		if(event->type() == QEvent::KeyPress)
+		{
+			QKeyEvent* keyEvent = static_cast<QKeyEvent*>(event);
+			if(keyEvent->key() == Qt::Key_Return || keyEvent->key() == Qt::Key_Enter)
+			{
+				QString currentInput = ui.cmdBar->getCurrentLine();
+				ui.cmdBar->pushCurrentLine();
+				return true;//stop Key return or Key enter
+			}
+		}
+	}
+	return QObject::eventFilter(watched, event);//normal processing
+}
+
+void MainWindow::about()
+{
+	QMessageBox Msgbox;
+	Msgbox.setWindowTitle("");
+    Msgbox.setText("TaskPad is a product created by Team F12-1C.\n"
+		"Members: ASHRAY, KAI, JIANGZE, THYAGESH, ZIXUAN.");
+    Msgbox.exec();
+}
+
+void MainWindow::createNewTaskTemplate()
+{
+	ui.cmdBar->createNewTaskTemplate();
+}
+
+void MainWindow::customisedUi(){
+	this->setWindowFlags(Qt::FramelessWindowHint);
+	this->setAttribute(Qt::WA_TranslucentBackground, true);
+
+	//magic number
+	ui.TaskList->setGeometry(9, 135, 595, ui.TaskList->topLevelItemCount() * 48 + 10);
+	ui.TaskList->header()->resizeSection(0, 70);
+	ui.TaskList->header()->resizeSection(1, 225);
+	ui.TaskList->setItemDelegate(new LastColumnDelegate(2));
+}
+
+void MainWindow::mousePressEvent(QMouseEvent *event){
+	this->windowPosition = this->pos();
+	this->mousePressPosition = event->globalPos();
+}
+
+void MainWindow::mouseMoveEvent(QMouseEvent *event){
+	this->mouseMovePosition = event->globalPos();
+	QPoint distanceToMove = this->mouseMovePosition - this->mousePressPosition + this->windowPosition;
+
+	this->move(distanceToMove);   
+}
