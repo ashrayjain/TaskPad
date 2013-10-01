0d8cb57ce56a38d8cfc65a10fded787e40a98cec
Thyagesh M <thyageshm@gmail.com>
2013-10-01 17:28:20 +0800

    Enum PERIOD_TYPE
    - added a new enum to handle the navigation functionality

--- TaskPad_library/Enum.h
+++ TaskPad_library/Enum.h
@@ -14,10 +14,14 @@ namespace TP
 	enum STATUS			{
 		ERROR, ERROR_INTERMEDIATE, SUCCESS, INTERMEDIATE, SUCCESS_INDEXED_COMMAND, DISPLAY
 	};
+	enum PERIOD_TYPE	{ DAY, WEEK, MONTH};
 
-	static string COMMAND_TYPE_STRING[8] = {"ADD","MOD","DEL","FIND","UNDO","REDO","SYNC","UNDEFINED"};
-	static string PRIORITY_STRING[3] = {"HIGH","MEDIUM","LOW"};
-	static string TASK_STATE_STRING[2] = {"UNDONE","DONE"};
-	static string TASK_TYPE_STRING[3] = {"TIMED","DEADLINE","FLOATING"};
+	//String representations of the Enums above
+
+	static const string COMMAND_TYPE_STRING[8] =	
+					{ "ADD","MOD","DEL","FIND","UNDO","REDO","SYNC","UNDEFINED"	};
+	static const string PRIORITY_STRING[3] =		{"HIGH","MEDIUM","LOW"};
+	static const string TASK_STATE_STRING[2] =		{"UNDONE","DONE"};
+	static const string TASK_TYPE_STRING[3] =		{"TIMED","DEADLINE","FLOATING"};
 }
 #endif
