2b1722576e36e6fb4daf457a712474e7c1ffc823
Thyagesh <Thyagesh@localhost>
2013-10-11 01:47:20 +0800

    Storage Class
    - load function is done!
            = waiting for executor to handle the incoming new tasks in the constructor

--- TaskPad_library/Storage.cpp
+++ TaskPad_library/Storage.cpp
@@ -3,27 +3,29 @@
 #include "Enum.h"
 #include <list>
 #include <sstream>
+#include <string>
 
 using namespace TP;
 
 const string Storage::_fileName = "TaskPad.txt";
-const string Storage::LABEL_TASK_COUNT = "Task Count: ";
-const string Storage::LABEL_NAME = "Name: ";
-const string Storage::LABEL_INDEX = "Index: ";
-const string Storage::LABEL_DUE_DATE = "Due: ";
-const string Storage::LABEL_FROM_DATE = "StartTime: ";
-const string Storage::LABEL_TO_DATE = "EndTime: ";
-const string Storage::LABEL_LOCATION = "Location: ";
-const string Storage::LABEL_PARTICIPANT = "Participant: ";
-const string Storage::LABEL_NOTE = "Note: ";
-const string Storage::LABEL_PRIORITY = "Priority: ";
-const string Storage::LABEL_TAG = "Tag: ";
-const string Storage::LABEL_REMINDER_TIME = "Reminder: ";
-const string Storage::LABEL_STATE = "State: ";
-const string Storage::LABEL_END_OF_TASK = "END OF TASK";
+const string Storage::LABEL_TASK_COUNT = "Task Count:";
+const string Storage::LABEL_NAME = "Name:";
+const string Storage::LABEL_INDEX = "Index:";
+const string Storage::LABEL_DUE_DATE = "Due:";
+const string Storage::LABEL_FROM_DATE = "StartTime:";
+const string Storage::LABEL_TO_DATE = "EndTime:";
+const string Storage::LABEL_LOCATION = "Location:";
+const string Storage::LABEL_PARTICIPANT = "Participant:";
+const string Storage::LABEL_NOTE = "Note:";
+const string Storage::LABEL_PRIORITY = "Priority:";
+const string Storage::LABEL_TAG = "Tag:";
+const string Storage::LABEL_REMINDER_TIME = "Reminder:";
+const string Storage::LABEL_STATE = "State:";
+const string Storage::LABEL_END_OF_TASK = "EndOfTask";
 
 Storage::Storage(list<Task>& taskList)
 {
+	this->load(taskList);
 }
 
 bool Storage::save(const list<Task>& taskList)
@@ -69,9 +71,10 @@ void Storage::closeTheReadFile()
 
 void Storage::saveTaskList(const list<Task>& taskList)
 {
+	/*
 	this->saveTaskCount(taskList.size());
 	writeLineToFile("");
-
+	*/
 	list<Task>::const_iterator it = taskList.begin();
 	while(it != taskList.end())
 	{
@@ -82,8 +85,9 @@ void Storage::saveTaskList(const list<Task>& taskList)
 
 void Storage::saveTask(const Task& task)
 {
+	writeLineToFile("");
 	this->saveTaskAttributes(task);
-	writeLineToFile(this->LABEL_END_OF_TASK);
+	writeLineToFile(this->LABEL_END_OF_TASK,false);
 	writeLineToFile("");
 	return;
 }
@@ -104,9 +108,9 @@ void Storage::saveTaskAttributes(const Task& tempTask)
 	this->saveState(tempTask);
 }
 
-void Storage::saveHeader(string headerStr)
+void Storage::saveLabel(string LabelStr)
 {
-	writeLineToFile(headerStr,false);
+	writeLineToFile(LabelStr,false);
 }
 
 void Storage::saveCount(unsigned long long count)
@@ -117,13 +121,13 @@ void Storage::saveCount(unsigned long long count)
 
 void Storage::saveTaskCount(unsigned long long taskCount)
 {
-	saveHeader(LABEL_TASK_COUNT);
+	saveLabel(LABEL_TASK_COUNT);
 	saveCount(taskCount);
 }
 
 void Storage::saveIndex(const Task& tempTask)
 {
-	saveHeader(LABEL_INDEX);
+	saveLabel(LABEL_INDEX);
 	this->writeLineToFile(convertToString(tempTask.getIndex()));
 }
 
@@ -131,7 +135,7 @@ void Storage::saveName(const Task& tempTask)
 {
 	if(tempTask.getFlagName())
 	{
-		saveHeader(LABEL_NAME);
+		saveLabel(LABEL_NAME);
 		this->writeLineToFile(tempTask.getName());
 	}
 }
@@ -140,7 +144,7 @@ void Storage::saveLocation(const Task& tempTask)
 {
 	if(tempTask.getFlagLocation())
 	{
-		saveHeader(LABEL_LOCATION);
+		saveLabel(LABEL_LOCATION);
 		this->writeLineToFile(tempTask.getLocation());
 	}
 }
@@ -155,7 +159,7 @@ void Storage::saveParticipants(const Task& tempTask)
 
 		while(pit != participantList.end())
 		{
-			saveHeader(LABEL_PARTICIPANT);
+			saveLabel(LABEL_PARTICIPANT);
 			participant = (*pit);//convertToString(tempTask.getParticipants());
 			this->writeLineToFile(participant);
 			pit++;
@@ -167,14 +171,14 @@ void Storage::saveNote(const Task& tempTask)
 {
 	if(tempTask.getFlagNote())
 	{
-		saveHeader(LABEL_NOTE);
+		saveLabel(LABEL_NOTE);
 		this->writeLineToFile(tempTask.getNote());
 	}
 }
 
 void Storage::savePriority(const Task& tempTask)
 {
-	saveHeader(LABEL_PRIORITY);
+	saveLabel(LABEL_PRIORITY);
 	string priorityStr = convertToString(tempTask.getPriority());
 	this->writeLineToFile(priorityStr);
 }	
@@ -189,7 +193,7 @@ void Storage::saveTags(const Task& tempTask)
 
 		while(tagit != tagsList.end())
 		{
-			saveHeader(LABEL_TAG);
+			saveLabel(LABEL_TAG);
 			tag = (*tagit);
 			this->writeLineToFile(tag);
 			tagit++;
@@ -206,7 +210,7 @@ void Storage::saveReminderTimes(const Task& tempTask)
 
 		while(rtit != reminderList.end())
 		{
-			saveHeader(LABEL_REMINDER_TIME);
+			saveLabel(LABEL_REMINDER_TIME);
 			string reminderStr = convertToString(*rtit);
 			this->writeLineToFile(reminderStr);
 			rtit++;
@@ -216,7 +220,7 @@ void Storage::saveReminderTimes(const Task& tempTask)
 
 void Storage::saveState(const Task& tempTask)
 {
-	saveHeader(LABEL_STATE);
+	saveLabel(LABEL_STATE);
 	string stateStr = convertToString(tempTask.getState());
 	this->writeLineToFile(stateStr);
 }
@@ -225,7 +229,7 @@ void Storage::saveDueDate(const Task& tempTask)
 {
 	if(tempTask.getFlagDueDate())
 	{
-		saveHeader(LABEL_DUE_DATE);
+		saveLabel(LABEL_DUE_DATE);
 		string dueDateStr = convertToString(tempTask.getDueDate());
 		this->writeLineToFile(dueDateStr);
 	}
@@ -235,7 +239,7 @@ void Storage::saveFromDate(const Task& tempTask)
 {
 	if(tempTask.getFlagFromDate())
 	{
-		saveHeader(LABEL_FROM_DATE);
+		saveLabel(LABEL_FROM_DATE);
 		string fromDateStr = convertToString(tempTask.getFromDate());
 		this->writeLineToFile(fromDateStr);
 	}
@@ -245,7 +249,7 @@ void Storage::saveToDate(const Task& tempTask)
 {
 	if(tempTask.getFlagToDate())
 	{
-		saveHeader(LABEL_TO_DATE);
+		saveLabel(LABEL_TO_DATE);
 		string toDateStr = convertToString(tempTask.getToDate());
 		this->writeLineToFile(toDateStr);
 	}
@@ -300,7 +304,7 @@ void Storage::writeLineToFile(string line, bool newLine)
 	if(newLine)
 		_fileWriter << line << endl;
 	else
-		_fileWriter << line;
+		_fileWriter << line << " ";
 }
 
 void Storage::emptyTheFile()
@@ -308,9 +312,198 @@ void Storage::emptyTheFile()
 	//throw "storage empty the file not implemented";
 }
 
-bool Storage::save(const Command& cmd)
+void Storage::loadTaskList(list<Task>& taskList)
 {
-	return false;
+	while(_fileReader.good())
+	{
+		taskList.push_back(this->getNextTask());
+		this->getNextLineFromFile();
+	}
+	return;
+}
+
+Task Storage::getNextTask()
+{
+	std::string newLine;
+	std::string newLabel;
+	std::string newData;
+	Task newTask;
+
+	while(_fileReader.good()) {
+		newLine = getNextLineFromFile();
+		newLabel = getNewLabel(newLine);
+		newData = getNewData(newLine);
+
+		if(newLabel == LABEL_END_OF_TASK) {
+			break;
+		}
+		else if(newLabel == LABEL_INDEX) {
+			newTask = createNewTask(getTaskIndex(newData));
+		}
+		else if(newLabel == LABEL_NAME) {
+			setTaskName(newTask, newData);
+		}
+		else if(newLabel == LABEL_DUE_DATE)	{
+			setTaskDueDate(newTask,newData);
+		}
+		else if(newLabel == LABEL_FROM_DATE) {
+			setTaskFromDate(newTask,newData);
+		}
+		else if(newLabel == LABEL_TO_DATE) {
+			setTaskToDate(newTask,newData);
+		}
+		else if(newLabel == LABEL_LOCATION)	{
+			setTaskLocation(newTask,newData);
+		}
+		else if(newLabel == LABEL_PARTICIPANT) {
+			setTaskParticipant(newTask,newData);
+		}
+		else if(newLabel == LABEL_NOTE) {
+			setTaskNote(newTask,newData);
+		}
+		else if(newLabel == LABEL_PRIORITY)	{
+			setTaskPriority(newTask,newData);
+		}
+		else if(newLabel == LABEL_TAG) {
+			setTaskTag(newTask,newData);
+		}
+		else if(newLabel == LABEL_REMINDER_TIME) {
+			setTaskReminderTime(newTask,newData);
+		}
+		else if(newLabel == LABEL_STATE) {
+			setTaskState(newTask,newData);
+		}
+	}
+
+	return newTask;
+}
+
+std::string Storage::getNewLabel(std::string newLine)
+{
+	std::string label;
+	stringstream tempStream(newLine);
+
+	tempStream >> label;
+
+	return label;
+}
+
+std::string Storage::getNewData(std::string newLine)
+{
+	int pos = newLine.find_first_of(" ");
+	return newLine.substr(pos+1);
+}
+
+unsigned long long Storage::getTaskIndex(std::string indexStr)
+{
+	return getIndexFromString(indexStr);
+}
+
+Task Storage::createNewTask(unsigned long long index) {
+	return Task(index);
+}
+void Storage::setTaskName	(Task& task,std::string taskName) {
+	task.setName(taskName);
+}
+void Storage::setTaskDueDate	(Task& task, std::string dueDateStr) {
+	time_t dueDate = getTimeFromString(dueDateStr);
+	task.setDueDate(dueDate);
+}
+void Storage::setTaskFromDate(Task& task, std::string fromDateStr) {
+	time_t fromDate = getTimeFromString(fromDateStr);
+	task.setFromDate(fromDate);
+}
+void Storage::setTaskToDate(Task& task, std::string toDateStr) {
+	time_t toDate = getTimeFromString(toDateStr);
+	task.setToDate(toDate);
+}
+void Storage::setTaskLocation(Task& task, std::string location) {
+	task.setLocation(location);
+}
+void Storage::setTaskParticipant(Task& task, std::string participant) {
+	task.setParticipants(participant,ADD_ELEMENT);
+}
+void Storage::setTaskNote(Task& task, std::string note) {
+	task.setNote(note);
+}
+void Storage::setTaskPriority(Task& task, std::string taskPriorityStr) {
+	PRIORITY taskPriority = getPriorityFromString(taskPriorityStr);
+	task.setPriority(taskPriority);
+}
+void Storage::setTaskTag(Task& task, std::string tag) {
+	task.setTags(tag,ADD_ELEMENT);
+}
+void Storage::setTaskReminderTime(Task& task, std::string reminderTimeStr) {
+	time_t reminderTime = getTimeFromString(reminderTimeStr);
+	task.setRemindTimes(reminderTime,ADD_ELEMENT);
+}
+void Storage::setTaskState(Task& task, std::string taskStateStr) {
+	TASK_STATE taskState = getTaskStateFromString(taskStateStr);
+	task.setState(taskState);
+}
+
+std::string Storage::getNextLineFromFile()
+{
+	std::string nextLine;
+	getline(_fileReader,nextLine);
+
+	return nextLine;
+}
+
+void Storage::load (list<Task>& taskList)
+{
+	this->openTheFileToRead();
+	this->getNextLineFromFile();
+	this->loadTaskList(taskList);
+	this->closeTheReadFile();
+	this->save(taskList);
+	return;
+}
+
+
+// From String converters
+
+int	Storage::getIntFromString (std::string attribute) {
+	stringstream tempStream(attribute);
+	int returnValue;
+
+	tempStream >> returnValue;
+
+	return returnValue;
+}
+unsigned long long Storage::getIndexFromString		(std::string attribute){
+	stringstream tempStream(attribute);
+	unsigned long long returnValue;
+
+	tempStream >> returnValue;
+
+	return returnValue;
+}
+time_t Storage::getTimeFromString (std::string attribute){
+	stringstream tempStream(attribute);
+	time_t returnValue;
+
+	tempStream >> returnValue;
+
+	return returnValue;
+}
+PRIORITY Storage::getPriorityFromString	(std::string attribute){
+	for (PRIORITY prio = HIGH; prio <= LOW; prio = static_cast<PRIORITY>(prio + 1))
+	{
+		if(attribute == PRIORITY_STRING[prio])
+		{
+			return prio;
+		}
+	}
+}
+TASK_STATE Storage::getTaskStateFromString	(std::string attribute) {
+	for (TASK_STATE state = UNDONE; state <= DONE; state = static_cast<TASK_STATE>(state + 1))
+	{
+		if(attribute == TASK_STATE_STRING[state])
+		{
+			return state;
+		}
+	}
 }
 
 Storage::~Storage()
