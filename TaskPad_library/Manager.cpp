e030fe881b6b2ab4e667b6f9c61f69847eb3867e
Zixuan <Zixuan@localhost>
2013-10-10 17:46:32 +0800

    latest

--- TaskPad_library/Manager.cpp
+++ TaskPad_library/Manager.cpp
@@ -38,15 +38,17 @@ Messenger Manager::processCommand(const string& newCommand) {
 
 void Manager::saveChanges()
 {
-	switch(this->_cmd->getCommandType())
-	{
-		case MOD:
-		case DEL:
-		case ADD:
-			this->_storage->save(this->_tasks);
-			break;
-		default:
-			break;
+	if(_cmd!=NULL){
+		switch(this->_cmd->getCommandType())
+		{
+			case MOD:
+			case DEL:
+			case ADD:
+				this->_storage->save(this->_tasks);
+				break;
+			default:
+				break;
+		}
 	}
 }
 
