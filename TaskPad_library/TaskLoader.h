a3361f3d3aa9435d37745e8c76b0b4b5c9a2e7f2
Thyagesh M <thyageshm@gmail.com>
2013-10-22 00:08:52 +0800

    Refactored the Unit Tests
    - test_messenger (100% coverage) and test_logger files (~96%)
    ---------------------------------------------------------------------------

---
+++ TaskPad_library/TaskLoader.h
@@ -0,0 +1,22 @@
+#ifndef _TASKLOADER_H_
+#define _TASKLOADER_H_
+
+#include <list>
+#include <fstream>
+#include "FileHandler.h"
+
+class Task;
+
+class TaskLoader: public FileHandler
+{
+	public:
+		virtual void load (std::list<Task>& taskList, const std::string& fileName) =0;
+		~TaskLoader() {}
+	protected:
+		virtual void openFile(const std::string& fileName, std::ios_base::openmode = std::ios::in) =0;
+		virtual void closeFile() =0;
+
+		std::ifstream _fileReader;
+};
+
+#endif
