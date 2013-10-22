92dcd7151a7bde5c97803e9b6cf0dd409e2bfef1
An Jiangze <anjiangze@gmail.com>
2013-10-22 19:05:40 +0800

    Interpreter V 0.25 fix deletion for index and set default time to 00:00

---
+++ TaskPad_library/quickadd_window.h
@@ -0,0 +1,28 @@
+#ifndef QUICKADD_WINDOW_H
+#define QUICKADD_WINDOW_H
+
+#include "./GeneratedFiles/ui_quickadd_window.h"
+#include "mainwindow.h"
+
+class QuickAddWindow: public QDialog
+{
+	Q_OBJECT
+
+public:
+	QuickAddWindow(QWidget *parent = 0);
+
+private:
+	void customisedUi();
+	bool eventFilter(QObject* watched, QEvent* event);
+	void mousePressEvent(QMouseEvent *event);
+	void mouseMoveEvent(QMouseEvent *event);
+
+	Ui::QuickAddWindowClass ui;
+	QPoint windowPosition;
+	QPoint mousePressPosition;
+	QPoint mouseMovePosition;
+	QString inputStr;
+	MainWindow* parentWindow;
+};
+
+#endif // QUICKADD_WINDOW_H
