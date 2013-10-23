efdf556c99146faff43a92f2c9d57faa2be7cc8b
An Jiangze <anjiangze@gmail.com>
2013-10-23 19:47:45 +0800

    Merge

---
+++ TaskPad_library/mainwindow.h
@@ -0,0 +1,70 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QSystemTrayIcon>
+#include <list>
+#include "Task.h"
+#include "Messenger.h"
+#include "./GeneratedFiles/ui_mainwindow.h"
+
+class CommandBar;
+class Manager;
+
+class MainWindow : public QMainWindow
+{
+	Q_OBJECT
+
+public:
+	MainWindow(QWidget *parent = 0);
+	~MainWindow(); 
+
+protected slots:
+	void about();
+	void help();
+	void getToday();
+	void getInbox();
+	void showWindow();
+	void showReminder();
+	void showQuickAddWindow();
+	void closeQuickAddWindow();
+	void handleQuickAddRequest(QString requestStr);
+	bool isCommandAdd(QString requestStr);
+	void iconIsActived(QSystemTrayIcon::ActivationReason);
+
+private:
+	void showTrayMsg(QString msg);
+	void keyPressEvent(QKeyEvent*event);
+	void reset();
+	void updateNavLabel(QString str);
+	void updateDetailsLabel(QString str);
+	void updateList(std::list<Task> result, bool isToday = false);
+	QTreeWidgetItem* extractTask(int index, Task task);
+	QTreeWidgetItem* extractTaskForToday(int index, Task task);
+	void clearDetails();
+	void updateDetails(Task t);
+	void updateStatusBar(QString str);
+	void handleGetToday(Messenger msg);
+	void handleGetInbox(Messenger msg);
+	void handleMessenger(Messenger msg);
+	void handleDisplay(Messenger msg);
+	void customisedUi();
+	void mousePressEvent(QMouseEvent *event);
+	void mouseMoveEvent(QMouseEvent *event);
+
+	QSystemTrayIcon* trayIcon;
+	Ui::MainWindowClass ui;
+	QPoint windowPosition;
+	QPoint mousePressPosition;
+	QPoint mouseMovePosition;
+	void changeEvent(QEvent* event);
+	bool eventFilter(QObject* watched, QEvent* event);
+
+	bool isQuickAddOpen;
+	QDialog* quickAddWindow;
+	Manager* scheduler;
+	QTimer* timer;
+
+	friend class QuickAddWindow;
+};
+
+#endif // MAINWINDOW_H
