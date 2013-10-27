2a7c97010a68137c26e6d351e109838b58dfbf26
Thyagesh M <thyageshm@gmail.com>
2013-10-27 18:21:48 +0800

    Manager class
    - solved issue 97
    - create new Command_find object for _lastSuccessfulFindCommand rather than point to same object
    - defensive coding in destructor

---
+++ TaskPad_library/FileHandler.h
@@ -0,0 +1,47 @@
+/*
+ * =====================================================================================
+ *
+ *		Filename:  FileHandler.h
+ *
+ *      Version:  1.0
+ *
+ *      Author:  Thyaegsh Manikandan (A0100124J)
+ *		Organization:  NUS, SoC
+ *
+ * =====================================================================================
+
+ ** Gist of File Contents:
+  *
+  * This header file defines an abstract class called FileHandler and all file handling classes
+  * in this program will inherit from this class
+  * 
+ */
+
+#ifndef _FILEHANDLER_H_
+#define _FILEHANDLER_H_
+
+#include<fstream>
+
+class FileHandler
+{
+	protected:
+		virtual void openFile(const std::string& fileName, std::ios_base::openmode) =0;
+		virtual void closeFile() =0;
+
+		static const std::string LABEL_START_OF_TASK;
+		static const std::string LABEL_NAME;
+		static const std::string LABEL_INDEX;
+		static const std::string LABEL_DUE_DATE;
+		static const std::string LABEL_FROM_DATE;
+		static const std::string LABEL_TO_DATE;
+		static const std::string LABEL_LOCATION;
+		static const std::string LABEL_PARTICIPANT;
+		static const std::string LABEL_NOTE;
+		static const std::string LABEL_PRIORITY;
+		static const std::string LABEL_TAG;
+		static const std::string LABEL_REMINDER_TIME;
+		static const std::string LABEL_STATE;
+		static const std::string LABEL_END_OF_TASK;
+};
+
+#endif
---
+++ TaskPad_library/LastColumnDelegate.h
@@ -0,0 +1,40 @@
+#include <QStyledItemDelegate>
+#include <QPainter>
+
+#ifndef LAST_COLUMN_DELEGATE_H
+#define LAST_COLUMN_DELEGATE_H
+
+class LastColumnDelegate: public QStyledItemDelegate
+{
+	Q_OBJECT
+
+public:
+	LastColumnDelegate(QObject *parent = 0)
+	: QStyledItemDelegate(parent)
+	{
+	}
+
+	void paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const
+	{
+		QStyleOptionViewItemV4 opt = option;
+		initStyleOption(&opt, index);
+
+		QStyledItemDelegate::paint(painter, option, index);
+		painter->save();
+
+		QString displayText = index.data().toString();
+		QTextOption alignment(Qt::AlignRight | Qt::AlignVCenter);
+		QPen penHText(QColor("#fff"));
+		painter->setPen(penHText);
+		QFont font1;
+		font1.setFamily(QStringLiteral("Segoe UI"));
+		font1.setPixelSize(16);
+		painter->setFont(font1);
+		QRect rect = opt.rect;
+		rect = rect.adjusted(18,0,-18,-3);
+		painter->drawText(rect, displayText, alignment);
+
+		painter->restore();
+	}
+};
+#endif
