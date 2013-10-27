8454a5757cad576c2b383f631446a8de7bde0f0a
XIE KAI <XIE KAI@localhost>
2013-10-28 02:46:21 +0800

    main refactoring of MainWindow. but quite buggy

--- TaskPad_library/mainwindow.h
+++ TaskPad_library/mainwindow.h
@@ -20,18 +20,20 @@ public:
 	~MainWindow(); 
 
 protected slots:
+	//Pop MsgBox
 	void about();
 	void help();
-	void getToday();
-	void getInbox();
+	//Window Display
 	void showWindow();
+	void iconIsActived(QSystemTrayIcon::ActivationReason);
 	void showReminder();
+	void handleShowReminder();
 	void showQuickAddWindow();
 	void closeQuickAddWindow();
 	void handleQuickAddRequest(QString requestStr);
-	void handleShowReminder();
-	bool isCommandAdd(QString requestStr);
-	void iconIsActived(QSystemTrayIcon::ActivationReason);
+	//Date Navigation
+	void getToday();
+	void getInbox();
 	void showNextDay();
 	void showNextWeek();
 	void showNextMonth();
@@ -40,40 +42,106 @@ protected slots:
 	void showPrevMonth();
 
 private:
+	//Init
+	void setupUI();
+	void setupDependency();
+	void setupScheduler();
+	void setupTrayIcon();
+	void setupTimer();
+	void setupHotkeys();
+	void setupConnection();
+	void installEventFilter();
+	void customisedUi();
+	//Destroy
+	void dispose();
+	//Tray Related
+	QString prepareTrayMsg( list<Task> &reminderList, QString output );
 	void showTrayMsg(QString msg, QString title = "TaskPad");
-	void keyPressEvent(QKeyEvent*event);
+	bool isCommandAdd(QString requestStr);
+	bool isEqualOne( QString &requestStr );
+	//View Related
 	void reset();
+	void updateStatusBar(QString str);
+	//--Task List Related
+	void updateMainView(  Messenger &msg, QString listTitle, QString statusBarLabel = "Ready" );
 	void updateNavLabel(QString str);
-	void updateDetailsLabel(QString str);
 	void updateList(std::list<Task> result);
 	QTreeWidgetItem* extractTask(int index, Task task);
+	QStringList extractFloating( int index, Task &task );
+	QStringList extractTimedDate( Task &task, QStringList strList, int index );
+	QStringList extractDueDate( Task &task, QStringList strList, int index );
+	void setNormalDelegate( int index );
+	void setHighPriorityDelegate( int index );
+	//--Details Related
+	void updateDetailsView( Messenger &msg, QString detailsLabel = "Task's Details");
+	void handleOneItemList( Messenger &msg, QString detailsLabel = "Task's Details" );
+	void updateDetailsLabel(QString str = "Task's Details");
 	void clearDetails();
-	void updateDetails(Task t);
-	void updateStatusBar(QString str);
+	void setDetailsViewEmpty();
+	void setDetailsViewOpacity40();
+	void setDetailsViewOpacity100();
+	void updateDetails(Task task);
+	void setNoteLabel( Task &task );
+	void setRemindTimesLabel( Task &task );
+	void setTagsLabel( Task &task );
+	void setParticipantsLabel( Task &task );
+	void setLocationLabel( Task &task );
+	void setDueOrFromToLabel( Task &task );
+	void setFloatingLabel();
+	void setTimedLabel( Task &task );
+	void setDueDateLabel( Task &task );
+	void setPriorityLabel( Task &task );
+	void setNameLabel( Task &task );
+	//Msg Handling
 	void handleGetToday(Messenger msg);//TODO: many handlers r redundant
 	void handleGetInbox(Messenger msg);
 	void handleMessenger(Messenger msg);
+	void handleMsg_SUCCESS_INDEXED_CMD( Messenger &msg );
+	void handleMsg_INTERMEDIATE( Messenger msg );
+	void handleMsg_SUCCESS( Messenger &msg );
+	void handleMsg_ERROR_INTERMEDIATE();
+	void handleMsg_ERROR( Messenger &msg );
 	void handleDisplay(Messenger msg);
 	void handleDateNavigation(TP::PERIOD_TYPE period, QString listTitle, bool isPrevious = false);
-	QString getTimePeriod(pair<tm, tm> period);
-	void customisedUi();
+	void updateForCmdExec( QString statusBarTxt, QString DetailsLabel, Messenger msg );
+	void refresh();
+	//Quick Add Window Related
+	void setupQuickAddConnection();
+	void setupQuickAddWindow();
+	void setIsQuickAddOpen(bool toggle);
+	void setFromReminder(bool toggle);
+	void setCurrRemindTime();
+	string getFindRtCmd();
+	void disposeQuickAddWindow();
+	void handleQA_DISPLAY( Messenger msg );
+	void handleQA_SUCCESS();
+	void handleQA_ERROR( QString requestStr, Messenger &msg );
+	QString getTimePeriodStr(pair<tm, tm> period);
+	//Other
+	void popMsgBox(QString title, QString description);
+	void keyPressEvent(QKeyEvent*event);
+	void changeEvent(QEvent* event);
+	void runInBackground(QEvent* event);
+	bool eventFilter(QObject* watched, QEvent* event);
 	void mousePressEvent(QMouseEvent *event);
 	void mouseMoveEvent(QMouseEvent *event);
 
-	QSystemTrayIcon* trayIcon;
+	//Main Widgets
 	Ui::MainWindowClass ui;
+	QSystemTrayIcon* trayIcon;
+	//State
+	QTimer* timer;
+	QDateTime currRemindTime;
+	QDialog* quickAddWindowObj;
 	QPoint windowPosition;
 	QPoint mousePressPosition;
 	QPoint mouseMovePosition;
-	void changeEvent(QEvent* event);
-	bool eventFilter(QObject* watched, QEvent* event);
-
+	//State Flag
 	bool isQuickAddOpen;
 	bool isFromReminder;
-	QDateTime currRemindTime;
-	QDialog* quickAddWindow;
+	//Logic's entrance
 	Manager* scheduler;
-	QTimer* timer;
+	
 
 	friend class QuickAddWindow;
 };
