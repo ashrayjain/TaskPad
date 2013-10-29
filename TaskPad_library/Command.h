9bc6d08a579534e3306e822321d2bebb9beabb1f
XIE KAI <XIE KAI@localhost>
2013-10-29 16:28:28 +0800

    command_base updates

--- TaskPad_library/Command.h
+++ TaskPad_library/Command.h
@@ -83,10 +83,15 @@ protected:
 	std::time_t				getToDate()				{ return _toDate;			}
 	std::string				getLocation()			{ return _location;			}
 	std::list<std::string>	getParticipants()		{ return _participants;		}
+	std::list<std::string>	getRemoveParticipants()	{ return _removeParticipants;}
+	std::list<std::string>	getAddParticipants()	{ return _addParticipants;	}
 	std::string				getNote()				{ return _note;				}
 	TP::PRIORITY			getPriority()			{ return _priority;			}
 	std::list<std::string>	getTags()				{ return _tags;				}
+	std::list<std::string>	getRemoveTags()			{ return _removeTags;		}
 	std::list<std::time_t>	getRemindTimes()		{ return _remindTime;		}
+	std::list<std::time_t>	getRemoveRemindTimes()	{ return _removeRemindTime;	}
+	std::list<std::time_t>	getAddRemindTimes()		{ return _addRemindTime;	}
 	TP::TASK_STATE			getTaskState()			{ return _taskState;		}
 	TP::TASK_TYPE			getTaskType()			{ return _taskType;			}
 	unsigned long long		getCreatedTime()		{ return _createdTime;		}
@@ -101,10 +106,15 @@ protected:
 	void setToDate(std::time_t toDate)				{ _toDate		= toDate;	FLAG_to			= TP::EDITED; }
 	void setLocation(std::string location)			{ _location		= location;	FLAG_location	= TP::EDITED; }
 	void setParticipants(std::list<std::string> ppl){ _participants	= ppl;		FLAG_participants= TP::EDITED; }
+	void setRemoveParticipants(std::list<std::string>ppl){_removeParticipants = ppl;FLAG_removeParticipants= TP::EDITED;}
+	void setAddParticipants(std::list<std::string>ppl){_addParticipants = ppl;	FLAG_addParticipants = TP::EDITED;}
 	void setNote(std::string note)					{ _note			= note;		FLAG_note		= TP::EDITED; }
 	void setPriority(TP::PRIORITY priority)			{ _priority		= priority;	FLAG_priority	= TP::EDITED; }
 	void setTags(std::list<std::string> tags)		{ _tags			= tags;		FLAG_tags		= TP::EDITED; }
+	void setRemoveTags(std::list<std::string> tags)	{ _removeTags	= tags;		FLAG_removeTags	= TP::EDITED; }
 	void setRemindTimes(std::list<std::time_t> time){ _remindTime	= time;		FLAG_remindTime	= TP::EDITED; }
+	void setRemoveRemindTimes(std::list<std::time_t>time){_removeRemindTime = time; FLAG_removeRemindTime = TP::EDITED;}
+	void setAddRemindTimes(std::list<std::time_t>time){_addRemindTime = time;	FLAG_addRemindTime = TP::EDITED;}
 	void setTaskState(TP::TASK_STATE state)			{ _taskState	= state;	FLAG_taskState	= TP::EDITED; }
 	void setTaskType(TP::TASK_TYPE type)			{ _taskType		= type;		FLAG_taskType	= TP::EDITED; }
 	void setCreatedTime(unsigned long long ctime)	{ _createdTime	= ctime;	FLAG_createdTime= TP::EDITED; }
@@ -117,14 +127,25 @@ protected:
 	bool getFlagName()								{ return FLAG_name;				}
 	bool getFlagOptName()							{ return FLAG_optName;			}
 	bool getFlagDue()								{ return FLAG_due;				}
+	bool getFlagRemoveDue()							{ return FLAG_removeDue;		}
 	bool getFlagFrom()								{ return FLAG_from;				}
+	bool getFlagRemoveFrom()						{ return FLAG_removeFrom;		}
 	bool getFlagTo()								{ return FLAG_to;				}
+	bool getFlagRemoveTo()							{ return FLAG_removeTo;			}
 	bool getFlagLocation()							{ return FLAG_location;			}
 	bool getFlagParticipants()						{ return FLAG_participants;		}
+	bool getFlagRemoveParticipants()				{ return FLAG_removeParticipants;}
+	bool getFlagAddParticipants()					{ return FLAG_addParticipants;	}
+	bool getFlagRemoveAllParticipants()				{ return FLAG_removeAllParticipants;}
 	bool getFlagNote()								{ return FLAG_note;				}
 	bool getFlagPriority()							{ return FLAG_priority;			}
 	bool getFlagTags()								{ return FLAG_tags;				}
+	bool getFlagRemoveTags()						{ return FLAG_removeTags;		}
+	bool getFlagRemoveAllTags()						{ return FLAG_removeAllTags;	}
 	bool getFlagRemindTimes()						{ return FLAG_remindTime;		}
+	bool getFlagRemoveRemindTimes()					{ return FLAG_removeRemindTime;	}
+	bool getFlagAddRemindTimes()					{ return FLAG_addRemindTime;	}
+	bool getFlagRemoveAllRemindTimes()				{ return FLAG_removeAllRemindTimes;}
 	bool getFlagTaskState()							{ return FLAG_taskState;		}
 	bool getFlagTaskType()							{ return FLAG_taskType;			}
 	bool getFlagCreatedTime()						{ return FLAG_createdTime;		}
@@ -132,6 +153,12 @@ protected:
 
 	//setter for flags
 	void setFlagExact()								{ FLAG_exact = TP::EDITED;		}
+	void setFlagRemoveDue()							{ FLAG_removeDue = TP::EDITED;	}
+	void setFlagRemoveFrom()						{ FLAG_removeFrom = TP::EDITED;	}
+	void setFlagRemoveTo()							{ FLAG_removeTo = TP::EDITED;	}
+	void setFlagRemoveAllTags()						{ FLAG_removeAllTags = TP::EDITED;}
+	void setFlagRemoveAllParticipants()				{ FLAG_removeAllParticipants = TP::EDITED;}
+	void setFlagRemoveAllRemindTimes()				{ FLAG_removeAllRemindTimes = TP::EDITED;}
 
 private:
 	//default values
@@ -153,14 +180,25 @@ private:
 	bool FLAG_name;
 	bool FLAG_optName;
 	bool FLAG_due;
+	bool FLAG_removeDue;
 	bool FLAG_from;
+	bool FLAG_removeFrom;
 	bool FLAG_to;
+	bool FLAG_removeTo;
 	bool FLAG_location;
 	bool FLAG_participants;
+	bool FLAG_removeParticipants;
+	bool FLAG_addParticipants;
+	bool FLAG_removeAllParticipants;
 	bool FLAG_note;
 	bool FLAG_priority;
 	bool FLAG_tags;
+	bool FLAG_removeTags;
+	bool FLAG_removeAllTags;
 	bool FLAG_remindTime;
+	bool FLAG_removeRemindTime;
+	bool FLAG_addRemindTime;
+	bool FLAG_removeAllRemindTimes;
 	bool FLAG_taskState;
 	bool FLAG_createdTime;
 	bool FLAG_taskType;
@@ -176,10 +214,15 @@ private:
 	std::time_t										_toDate;
 	std::string										_location;
 	std::list<std::string>							_participants;
+	std::list<std::string>							_removeParticipants;
+	std::list<std::string>							_addParticipants;
 	std::string										_note;
 	TP::PRIORITY									_priority;//high | medium | low
 	std::list<std::string>							_tags;
+	std::list<std::string>							_removeTags;
 	std::list<std::time_t>							_remindTime;
+	std::list<std::time_t>							_removeRemindTime;
+	std::list<std::time_t>							_addRemindTime;
 	std::string										_syncProviderName;
 	TP::TASK_STATE									_taskState;//done | undone
 	unsigned long long								_createdTime;
