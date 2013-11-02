d6b546326ceb3b5cd17d99bf779e2eb80c09ea03
Thyagesh M <thyageshm@gmail.com>
2013-11-02 22:36:40 +0800

    Enum.h - added a new task_state: OVERDUE - included the string version of it in the TASK_STATE_STRING array

--- TaskPad_library/Enum.h
+++ TaskPad_library/Enum.h
@@ -5,14 +5,14 @@
 
 namespace TP{
 
-	static const int COMMAND_TYPE_COUNT = 8;
-	static const int PRIORITY_COUNT = 3;
-	static const int TASK_STATE_COUNT = 2;
-	static const int TASK_TYPE_COUNT = 3;
-	static const int  LOG_TYPE_COUNT = 5;
+	static const int COMMAND_TYPE_COUNT		= 8;
+	static const int PRIORITY_COUNT			= 3;
+	static const int TASK_STATE_COUNT		= 3;
+	static const int TASK_TYPE_COUNT		= 3;
+	static const int LOG_TYPE_COUNT			= 5;
     enum COMMAND_TYPE	{ ADD, MOD, DEL, FIND, INDEX, UNDO, REDO, SYNC, UNDEFINED};
     enum PRIORITY		{ HIGH, MEDIUM, LOW, NON_HIGH };
-    enum TASK_STATE		{ UNDONE = false, DONE = true };
+    enum TASK_STATE		{ UNDONE, OVERDUE, DONE };
     enum TASK_TYPE		{ TIMED, DEADLINE, FLOATING };
     enum CMD_PROPERTY	{ UNCHANGED = false, EDITED = true };
     enum STATUS			{
@@ -26,7 +26,7 @@ namespace TP{
 	static const std::string COMMAND_TYPE_STRING[COMMAND_TYPE_COUNT] =	
                     { "ADD","MOD","DEL","FIND","UNDO","REDO","SYNC","UNDEFINED"	};
 	static const std::string PRIORITY_STRING[PRIORITY_COUNT]		=		{"HIGH","MEDIUM","LOW"};
-	static const std::string TASK_STATE_STRING[TASK_STATE_COUNT]	=		{"UNDONE","DONE"};
+	static const std::string TASK_STATE_STRING[TASK_STATE_COUNT]	=		{"UNDONE","OVERDUE","DONE"};
 	static const std::string TASK_TYPE_STRING[TASK_TYPE_COUNT]		=		{"TIMED","DEADLINE","FLOATING"};
 	static const std::string LOG_TYPE_STRING[LOG_TYPE_COUNT]		=		{"INFOLOG","NOTICELOG","WARNINGLOG","ERRORLOG","FATALLOG"};
 }
