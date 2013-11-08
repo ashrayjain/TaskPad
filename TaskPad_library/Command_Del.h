72ee18d4a3ab585c23749bdf2bc80f4c38b78d3b
Thyagesh M <thyageshm@gmail.com>
2013-11-08 16:01:48 +0800

    handle the pulling down of 'from' and pushing up of 'to'

---
+++ TaskPad_library/Command_Del.h
@@ -0,0 +1,39 @@
+#ifndef TASKPAD_COMMAND_DEL_H
+#define TASKPAD_COMMAND_DEL_H
+
+/*
+ *=======================
+ 
+ Command Del
+ 
+ *=======================
+ */
+
+#include "Command.h"
+
+class Command_Del: public Command
+{
+public:
+	Command_Del()	{ init(); setCommandType(TP::DEL); }
+	~Command_Del()	{};
+	
+	//getter for fields
+	int							getIndex()			{ return Command::getIndex();		}
+	std::string					getName()			{ return Command::getName();		}
+	unsigned long long			getCreatedTime()	{ return Command::getCreatedTime();	}
+	
+	//setter for fields
+	void setIndex(int idx)							{ Command::setIndex(idx);			}
+	void setName(std::string name)					{ Command::setName(name);			}
+	void setCreatedTime(unsigned long long ctime)	{ Command::setCreatedTime(ctime);	}
+	
+	//getter for flags
+	bool getFlagIndex()								{ return Command::getFlagIndex();		}
+	bool getFlagExact()								{ return Command::getFlagExact();		}
+	bool getFlagCreatedTime()						{ return Command::getFlagCreatedTime(); }
+	
+	//setter for flags
+	void setFlagExact()								{ Command::setFlagExact();				}
+};
+
+#endif
---
+++ TaskPad_library/Executor_Del.cpp
@@ -0,0 +1,94 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Executor_Del.cpp
+ *
+ *        Version:  1.0
+ *        Created:  11/02/13 13:47:00
+ *
+ *         Author:  ASHRAY JAIN (A0105199B), ashrayj11@gmail.com
+ *   Organization:  NUS, SoC
+ *
+ * =====================================================================================
+ */
+
+#include "Executor_Del.h"
+
+void Executor_Del::executeCommand(Command* cmd, Messenger &response, Datastore &ds) {
+	Command_Del* delCmd = dynamic_cast<Command_Del*>(cmd);
+	if (delCmd->getFlagCreatedTime())
+		deleteTaskByIndex(delCmd->getCreatedTime(), response, ds);
+	else
+		deleteTaskByName(delCmd->getName(), response, delCmd->getFlagExact(), ds);
+}
+
+void Executor_Del::deleteTaskByIndex(const unsigned long long &index, Messenger &response, Datastore &ds) {
+	bool indexFound = false;
+	list<Task> tasks = ds.getTaskList();
+	for(Datastore::const_iterator i = ds.cbegin(); i != ds.cend() && !indexFound; i++)
+	//for(list<Task>::iterator i = tasks.begin(); i != tasks.end() && !indexFound; ++i)
+		if (i->getIndex() == index) {
+			setOpSuccessTask(*i, response);
+			ds.deleteTask(i);
+			indexFound = true;
+			break;
+		}
+
+	if (!indexFound)
+		setIndexNotFound(index, response);
+}
+
+void Executor_Del::deleteTaskByName(const string &name, Messenger &response, const bool &exactFlag, Datastore &ds) {
+	if (exactFlag)
+		deleteByExactName(name, response, ds);
+	else
+		deleteByApproxName(name, response, ds);
+}
+
+void Executor_Del::deleteByExactName(const string &name, Messenger &response, Datastore &ds) {
+	bool nameFound = false;
+	list<Task> tasks = ds.getTaskList();
+	for(Datastore::const_iterator* i = ds.cbeginPtr(); i != ds.cendPtr() && !nameFound; ++(*i))
+	//for(list<Task>::iterator i = tasks.begin(); i != tasks.end() && !nameFound; ++i)
+		if ((*i)->getName() == name) {
+			setOpSuccessTask(*(*i), response);
+			ds.deleteTask((*i));//distance(tasks.begin(), i));			
+			nameFound = true;
+			break;
+		}
+
+	if (!nameFound)
+		setNameNotFound(name, response);
+}
+
+void Executor_Del::deleteByApproxName(const string &name, Messenger &response, Datastore &ds) {
+	list<Task> matchingResults;
+	list<Task> caseInsensitiveResults;
+	list<Task> tasks = ds.getTaskList();
+	string lowerName = getLowerStr(name);
+	for(Datastore::const_iterator i = ds.cbegin(); i != ds.cend(); i++) {
+	//for(list<Task>::iterator i = tasks.begin(); i != tasks.end(); ++i) {
+		string currName = getLowerStr(i->getName());
+		if (currName.find(name) != string::npos)
+			matchingResults.push_back(Task(*i));
+		else if (currName.find(lowerName) != string::npos)
+			caseInsensitiveResults.push_back(Task(*i));
+	}
+	selectAppropriateDeleteResponse(matchingResults, caseInsensitiveResults, name, response, ds);
+}
+
+void Executor_Del::selectAppropriateDeleteResponse(const list<Task> &matchingResults, const list<Task> &caseInsensitiveResults, 
+												   const string &name, Messenger &response, Datastore &ds) {
+	if (matchingResults.size() == EMPTY_LIST_SIZE) {
+		if (caseInsensitiveResults.size() == EMPTY_LIST_SIZE)
+			setNameNotFound(name, response);
+		else if (caseInsensitiveResults.size() == SINGLE_RESULT_LIST_SIZE)
+			deleteTaskByIndex(caseInsensitiveResults.front().getIndex(), response, ds);
+		else
+			setOpIntermediateTaskList(caseInsensitiveResults, response);
+	}
+	else if (matchingResults.size() == SINGLE_RESULT_LIST_SIZE)
+		deleteTaskByIndex(matchingResults.front().getIndex(), response, ds);
+	else
+		setOpIntermediateTaskList(matchingResults, response);
+}
