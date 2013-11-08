6b7ba147e1c7376e18a5063e72a23860ac269418
XIE KAI <XIE KAI@localhost>
2013-11-08 21:54:57 +0800

    refactoring highlighter

--- TaskPad_library/Highlighter.h
+++ TaskPad_library/Highlighter.h
@@ -8,59 +8,21 @@ class Highlighter: public QSyntaxHighlighter
 public:
 	enum HIGHLIGHT_TYPE { COMMAND = 0, KEYWORD = 1, PAIR_QUOTE_LEFT = 2, BLANK = 3 };
 
-	Highlighter(QTextDocument *parent)
-	:QSyntaxHighlighter(parent)
-	{
-		CommandFormat.setFontWeight(QFont::Bold);
-		CommandFormat.setForeground(QBrush("#232323"));
-		KeywordFormat.setFontWeight(QFont::Bold);
-		KeywordFormat.setForeground(QBrush("#397CD4"));
-		PairOfQuoteLeft.setFontWeight(QFont::Bold);
-		PairOfQuoteLeft.setForeground(QBrush("#787878"));
-		BlankFormat.setFontWeight(QFont::Bold);
-		BlankFormat.setForeground(QBrush("#742894"));
-
-		addRegex(COMMAND, "^(add|mod|del|find|undo|redo)");
-		addRegex(KEYWORD, " exact| name| by| due| from| to| location| place| at| with| ppl| note| priority| impt"\
-						"| remind| rt| overdue| done| undone| deadline| timed| floating| #| -by| -due"\
-						"| -from| -to| -with(all|[^a])| -ppl(all|[^a])| -remind(all|[^a])| -rt(all|[^a])| -#| \\+with| \\+ppl| \\+remind| \\+rt| \\+#");
-		addRegex(PAIR_QUOTE_LEFT, "`(.*)`");
-		addRegex(BLANK, "__[A-Z]+__");
-	}
-
-	void highlightBlock(const QString &text)
-	{
-		highlightPatterns(text);
-	}
+	Highlighter(QTextDocument *parent);
+	void highlightBlock(const QString &text){  highlightPatterns(text);   }
 
 private:
-	void addRegex(HIGHLIGHT_TYPE type, const QString &pattern, bool minimal = true)
-	{
-		QRegExp regex(pattern);
-		regex.setPatternSyntax(QRegExp::RegExp2);
-		regex.setMinimal(minimal);
-		regexForType.push_back(regex);
-	}
+	static const char* COLOR_BLACK;
+	static const char* COLOR_BLUE;
+	static const char* COLOR_GRAY;
+	static const char* COLOR_PURPLE;
+	static const char* REGEX_COMMAND;
+	static const char* REGEX_KEYWORD;
+	static const char* REGEX_QUOTE_LEFT_PAIRS;
+	static const char* REGEX_HOTKEY_TEMPLATE_BLANK;
 
-	void highlightPatterns(const QString &text)
-	{
-		for(int i = 0; i < regexForType.size(); i++){//foreach regex
-			const QRegExp &regex = regexForType[i];
-			int index = regex.indexIn(text);
-			while (index > -1) {//foreach regex's occurrence
-				int length = regex.matchedLength();
-				if (i == COMMAND)
-					setFormat(index, length, CommandFormat);
-				else if (i == KEYWORD)
-					setFormat(index, length, KeywordFormat);
-				else if (i == PAIR_QUOTE_LEFT)
-					setFormat(index, length, PairOfQuoteLeft);
-				else if (i == BLANK)
-					setFormat(index, length, BlankFormat);
-				index = regex.indexIn(text, index + length);
-			}
-		}
-	}
+	void addRegex(HIGHLIGHT_TYPE type, const QString &pattern, bool minimal = true);
+	void highlightPatterns(const QString &text);
 
 	QTextCharFormat CommandFormat;
 	QTextCharFormat KeywordFormat;
