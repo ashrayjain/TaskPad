1342c4deb99284b81a3a126a2e8e894b89930921
XIE KAI <XIE KAI@localhost>
2013-11-08 21:19:41 +0800

    refactoring listItemDelegate

--- TaskPad_library/ListItemDelegate.cpp
+++ TaskPad_library/ListItemDelegate.cpp
@@ -1,3 +1,20 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  ListItemDelegate.cpp
+ *
+ *           Note:  ListItemDelegate class makes item displayed specially for different
+ *	task priority and task state
+ *
+ *        Version:  1.0
+ *        Created:  10/21/13 16:13:09
+ *
+ *         Author:  XIE KAI (A0102016E), gigikie@gmail.com
+ *   Organization:  NUS, SoC
+ *
+ * =====================================================================================
+ */
+
 #include "ListItemDelegate.h"
 
 const int ListItemDelegate::_1ST_COLUMN = 0;
@@ -5,6 +22,7 @@ const int ListItemDelegate::_2ND_COLUMN = 1;
 const int ListItemDelegate::_3RD_COLUMN = 2;
 const QColor ListItemDelegate::WHITE_COLOR = "#ffffff";
 const QColor ListItemDelegate::RED_COLOR = "#FFB4B9";
+const QColor ListItemDelegate::ORANGE_COLOR = "#FDEBA3";
 const QString ListItemDelegate::FONT_SEGOE_UI = QStringLiteral("Segoe UI");
 
 ListItemDelegate::ListItemDelegate( TP::PRIORITY priority, TP::TASK_STATE state, QObject *parent /*= 0*/ ) : QStyledItemDelegate(parent){
@@ -13,32 +31,65 @@ ListItemDelegate::ListItemDelegate( TP::PRIORITY priority, TP::TASK_STATE state,
 }
 
 void ListItemDelegate::alignRight( const QModelIndex &index, QPainter * painter, QStyleOptionViewItemV4 &opt ) const{
+	const int DESIRED_FONT_SIZE = 15;
+	const int X_PT1 = 18, Y_PT1 = 0, X_PT2 = -18, Y_PT2 = -3;
+	
 	QString displayText = index.data().toString();
 	QTextOption alignment(Qt::AlignRight | Qt::AlignVCenter);
 	QFont font;
 	font.setFamily(FONT_SEGOE_UI);
-	font.setPixelSize(15);
-	QRect rect = opt.rect;
-	rect = rect.adjusted(18,0,-18,-3);
+	font.setPixelSize(DESIRED_FONT_SIZE);
+	QRect drawingRegion = opt.rect;
+	drawingRegion = drawingRegion.adjusted(X_PT1, Y_PT1, X_PT2, Y_PT2);
 	painter->setFont(font);
 	painter->setPen(WHITE_COLOR);
-	painter->drawText(rect, displayText, alignment);
+	painter->drawText(drawingRegion, displayText, alignment);
 }
 
 void ListItemDelegate::paintStrikeOut( QPainter * painter, QStyleOptionViewItemV4 &opt ) const{
-	QRect rect = opt.rect;
-	rect.adjust(5,24,-5,-24);
+	const int X_PT1 = 5, Y_PT1 = 24, X_PT2 = -5, Y_PT2 = -24;
+
+	QRect drawingRegion = opt.rect;
+	drawingRegion.adjust(X_PT1, Y_PT1, X_PT2, Y_PT2);
 	painter->setPen(WHITE_COLOR);
 	painter->setBrush(QBrush(WHITE_COLOR));
-	painter->drawRect(rect);
+	painter->drawRect(drawingRegion);
 }
 
 void ListItemDelegate::paintHighPrioBar( QPainter * painter, QStyleOptionViewItemV4 &opt ) const{
-	QRect rect = opt.rect;
-	rect.adjust(-5,0,-52,-2);
+	const int X_PT1 = -5, Y_PT1 = 0, X_PT2 = -52, Y_PT2 = -2;
+
+	QRect drawingRegion = opt.rect;
+	drawingRegion.adjust(X_PT1, Y_PT1, X_PT2, Y_PT2);
 	painter->setPen(RED_COLOR);
 	painter->setBrush(QBrush(RED_COLOR));
-	painter->drawRect(rect);
+	painter->drawRect(drawingRegion);
+}
+
+void ListItemDelegate::paintForOverdue( QPainter * painter ) const{
+	const int DESIRED_FONT_SIZE = 18;
+	const int X_PT1 = 0, Y_PT1 = 0, X_PT2 = 0, Y_PT2 = -2;
+	QString displayTextForOVERDUE = "!!";
+	
+	QTextOption alignment(Qt::AlignLeft | Qt::AlignVCenter);
+	QFont font;
+	font.setFamily(FONT_SEGOE_UI);
+	font.setPixelSize(DESIRED_FONT_SIZE);
+	QRect drawingRegion = _opt.rect;
+	drawingRegion = drawingRegion.adjusted(X_PT1, Y_PT1, X_PT2, Y_PT2);
+	painter->setFont(font);
+	painter->setPen(WHITE_COLOR);
+	painter->drawText(drawingRegion, displayTextForOVERDUE, alignment);
+}
+
+void ListItemDelegate::paintMediumPrioBar( QPainter * painter ) const{
+	const int X_PT1 = -5, Y_PT1 = 0, X_PT2 = -52, Y_PT2 = -2;
+
+	QRect drawingRegion = _opt.rect;
+	drawingRegion.adjust(X_PT1, Y_PT1,X_PT2,Y_PT2);
+	painter->setPen(ORANGE_COLOR);
+	painter->setBrush(QBrush(ORANGE_COLOR));
+	painter->drawRect(drawingRegion);
 }
 
 void ListItemDelegate::setupPainting( const QStyleOptionViewItem & option, const QModelIndex & index, QPainter * painter ) const{
@@ -49,6 +100,8 @@ void ListItemDelegate::setupPainting( const QStyleOptionViewItem & option, const
 	QStyledItemDelegate::paint(painter, _opt, index);
 }
 
+//must have this function
+//which will be called by MainWindow
 void ListItemDelegate::paint( QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index ) const{
 	setupPainting(option, index, painter);
 	painter->save();
@@ -56,26 +109,13 @@ void ListItemDelegate::paint( QPainter *painter, const QStyleOptionViewItem &opt
 		paintHighPrioBar(painter, _opt);
 	}
 	else if(index.column() == _1ST_COLUMN && _taskPriority == TP::MEDIUM){
-		QRect rect = _opt.rect;
-		rect.adjust(-5,0,-52,-2);
-		painter->setPen("#FDEBA3");
-		painter->setBrush(QBrush("#FDEBA3"));
-		painter->drawRect(rect);
+		paintMediumPrioBar(painter);
 	}
 	else if(index.column() == _2ND_COLUMN && _taskState == TP::DONE){
 		paintStrikeOut(painter, _opt);
 	}
 	else if(index.column() == _2ND_COLUMN && _taskState == TP::OVERDUE){
-		QString displayText = "!!";
-		QTextOption alignment(Qt::AlignLeft | Qt::AlignVCenter);
-		QFont font;
-		font.setFamily(FONT_SEGOE_UI);
-		font.setPixelSize(18);
-		QRect rect = _opt.rect;
-		rect = rect.adjusted(0,0,0,-2);
-		painter->setFont(font);
-		painter->setPen(WHITE_COLOR);
-		painter->drawText(rect, displayText, alignment);
+		paintForOverdue(painter);
 	}
 	else if(index.column() == _3RD_COLUMN){
 		alignRight(index, painter, _opt);
--- TaskPad_library/ListItemDelegate.h
+++ TaskPad_library/ListItemDelegate.h
@@ -1,3 +1,20 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  ListItemDelegate.h
+ *
+ *           Note:  ListItemDelegate class makes item displayed specially for different
+ *	task priority and task state
+ *
+ *        Version:  1.0
+ *        Created:  10/21/13 16:13:09
+ *
+ *         Author:  XIE KAI (A0102016E), gigikie@gmail.com
+ *   Organization:  NUS, SoC
+ *
+ * =====================================================================================
+ */
+
 #include <QStyledItemDelegate>
 #include <QPainter>
 #include "Enum.h"
@@ -5,8 +22,8 @@
 #ifndef HIGH_PRIORITY_DELEGATE_H
 #define HIGH_PRIORITY_DELEGATE_H
 
-class ListItemDelegate: public QStyledItemDelegate
-{
+class ListItemDelegate: public QStyledItemDelegate{
+
 	Q_OBJECT
 
 public:
@@ -19,11 +36,14 @@ private:
 	static const int _3RD_COLUMN;
 	static const QColor WHITE_COLOR;
 	static const QColor RED_COLOR;
+	static const QColor ORANGE_COLOR;
 	static const QString FONT_SEGOE_UI;
 
 	void setupPainting( const QStyleOptionViewItem & option, const QModelIndex & index, QPainter * painter ) const;
 	void paintHighPrioBar( QPainter * painter, QStyleOptionViewItemV4 &opt ) const;
+	void paintMediumPrioBar( QPainter * painter ) const;
 	void paintStrikeOut( QPainter * painter, QStyleOptionViewItemV4 &opt ) const;
+	void paintForOverdue( QPainter * painter ) const;
 	void alignRight( const QModelIndex &index, QPainter * painter, QStyleOptionViewItemV4 &opt ) const;
 
 	mutable QStyleOptionViewItemV4 _opt;
