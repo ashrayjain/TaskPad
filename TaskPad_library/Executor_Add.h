d67a637925f9a713e8906ea06e9bc8ed442adb58
Ashray Jain <ashrayj11@gmail.com>
2013-11-09 17:56:00 +0800

    Added std::

--- TaskPad_library/Executor_Add.h
+++ TaskPad_library/Executor_Add.h
@@ -24,11 +24,13 @@ private:
 	static const unsigned RT_MIN_M_ARR[];
 	static const unsigned RT_MIN_L_ARR[];
 
-	bool			validAddCmd						(Command_Add* cmd, Messenger &response);
-	Task			formTaskFromAddCmd				(Command_Add* cmd);
-	void			setDefaultRemindTimes			(Task &task);
-	void			setDefaultRemindTimesPriorityH	(Task &task);
-	void			setDefaultRemindTimesPriorityM	(Task &task);
-	void			setDefaultRemindTimesPriorityL	(Task &task);
-	list<time_t>	getRemindTimesFromMinutesBefore	(const unsigned minutesBeforeList[], const int N, const time_t &deadline) const;
+	bool					validAddCmd						(Command_Add* cmd, Messenger &response);
+	Task					formTaskFromAddCmd				(Command_Add* cmd);
+	void					setDefaultRemindTimes			(Task &task);
+	void					setDefaultRemindTimesPriorityH	(Task &task);
+	void					setDefaultRemindTimesPriorityM	(Task &task);
+	void					setDefaultRemindTimesPriorityL	(Task &task);
+	std::list<std::time_t>	getRemindTimesFromMinutesBefore	(const unsigned minutesBeforeList[], 
+															 const int N, 
+															 const std::time_t &deadline) const;
 };
