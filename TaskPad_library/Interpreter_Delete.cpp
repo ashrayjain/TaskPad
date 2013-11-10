abf70a48f9ae872733f65e764660c584d5d39425
Thyagesh M <thyageshm@gmail.com>
2013-11-10 21:36:08 +0800

    Added Unit test for TaskSaver's method- save() :D

---
+++ TaskPad_library/Interpreter_Delete.cpp
@@ -0,0 +1,20 @@
+#include "Interpreter_Delete.h"
+
+using namespace TP;
+using namespace std;
+
+Command* Interpreter_Delete:: interpretDelete(Command_Del*commandType,string commandStr, Messenger &response, bool &flag){
+
+	stringstream extractContent(commandStr);
+	string content;
+	getline(extractContent,content,NOTATION_ACCENT_GRAVE);
+	content.clear();
+	getline(extractContent,content,NOTATION_ACCENT_GRAVE);
+	commandType->setName(content);
+
+
+	response.setCommandType(DEL);
+	response.setStatus(SUCCESS);
+	return (Command*)commandType;
+
+}
